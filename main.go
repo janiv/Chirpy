9eb9e3b53457b208d37afda5e14000209bb8d2e8
janiv <jani.vilkki@gmail.com>
2025-06-03 11:09:51 -0500

    ch4l2 next

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -11,13 +24,31 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w,
+		`<html>
+		<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+		</body>
+		</html>`,
+		cfg.fileserverHits.Load())
+}
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
-
+	apiCfg := apiConfig{}
 	serve_mux := http.NewServeMux()
-	serve_mux.HandleFunc("/healthz", handlerReadiness)
-	serve_mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	serve_mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	serve_mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+	serve_mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serve_mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	server := http.Server{
 		Handler: serve_mux,
